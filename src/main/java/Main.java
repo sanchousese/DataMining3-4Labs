bb035a8d641883ee3f1ac5eb3584b4208dbdf684
sanchousese <saniasutula@gmail.com>
2015-04-22 00:22:59 +0300

    File path was fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ import java.io.FileReader;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(
-                new FileReader("/home/sancho/IdeaProjects/work/untitled3/data/boobs_dataset_1.arff"));   //Тут змінити на файл з даними
+                new FileReader("../untitled3/data/boobs_dataset_1.arff"));   //Тут змінити на файл з даними
         Instances data = new Instances(reader);
         reader.close();
         data.setClassIndex(data.numAttributes() - 1);
